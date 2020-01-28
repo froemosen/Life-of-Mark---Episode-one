fbecda02b5c6ea2f31a2c72112233803d91f9d97
froemosen <60390711+froemosen@users.noreply.github.com>
2020-01-28 18:28:34 +0100

    Den kode som Mads startede på i dag

---
+++ Startup.py
@@ -0,0 +1,18 @@
+#pip install playsound alle!
+import pygame as pg
+
+pg.init()
+win = pg.display.set_mode((0,0), pg.FULLSCREEN)
+start = True
+
+class mark(object):
+    def __init__(self):
+        pass
+    def draw():
+        pass
+class map(object):
+    def __init__(self):
+        pass
+    def draw():
+        pass
+
